05aae9a59c39b848dd889129a89d15ee2d37ca77
wudi <wudi2852987@163.com>
2015-04-03 14:48:54 +0800

    修改下载添加html

--- routes/downFiels.js
+++ routes/downFiels.js
@@ -9,20 +9,23 @@ router.get('/', function(req, res, next) {
    var _fontname =req.query.fontname;
 
    var fontspath = path.resolve(__dirname, '../public/temp/'+_fontname+'/'+_fontname+'');
+
+   var demopath = path.resolve(__dirname, '../views/');
     
    var archive = new zip(); 
 	 
-	archive.addFiles([  
-	    { name: _fontname+".eot", path: fontspath+".eot" }, 
-	    { name: _fontname+".svg", path: fontspath+".svg" }, 
-	    { name: _fontname+".ttf", path: fontspath+".ttf" }, 
-	    { name: _fontname+".woff", path: fontspath+".woff" }
+	archive.addFiles([ 
+		{ name:"demo.html", path:demopath+'/demo.html'},  
+	    { name:"webfont.eot", path: fontspath+".eot" }, 
+	    { name:"webfont.svg", path: fontspath+".svg" }, 
+	    { name:"webfont.ttf", path: fontspath+".ttf" }, 
+	    { name:"webfont.woff", path: fontspath+".woff" }
 	], function (err) { 
 	    if (err) return console.log("err while adding files", err); 
 	 
 	    var buff = archive.toBuffer(); 
 	 
-	    fs.writeFile(fontspath+".zip", buff, function () { 
+	    fs.writeFile(fontspath+"webfont.zip", buff, function () { 
 	        console.log("Finished");   
 	        res.download(fontspath+".zip");
 	    }); 
